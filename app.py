ddcf23d375702c01d54390d120ff2d5731553e3e
ryukh1001 <ryukh1001@naver.com>
2021-03-04 23:46:04 +0900

    v0.0.3

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import c3d
 
 file_name = 'Trial02.c3d'
-path = 'datasets/' + file_name
+path = 'dataset/' + file_name
 
 header = c3d.Header(open(path, 'rb'))
 frame_rate = header.frame_rate
@@ -16,7 +16,9 @@ for i, points, analog in reader.read_frames():
     frames.append(result.tolist())
 
 ## Flask ##
-from flask import Flask, render_template
+from flask import Flask, render_template, request, send_file
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)   # Flask 객체 인스턴스 생성
 
@@ -24,7 +26,21 @@ app = Flask(__name__)   # Flask 객체 인스턴스 생성
 def index():
     return render_template('index.html', frame_rate=frame_rate, frames=frames)
 
+@app.route('/upload', methods=['POST']) # 접속하는 url
+def upload_file():  # 동시에 이름이 같은 파일을 2개 이상 업로드 하는 경우 문제 발생 (해결방법?)
+    if request.method == 'POST':
+        f = request.files['file']
+        # 저장할 경로 + 파일명
+        f.save(os.path.join("uploads", secure_filename(f.filename)))
+        return '파일 업로드 성공!'
+
+@app.route('/download', methods=['POST']) # 접속하는 url
+def download_sample_file():
+    sample_file = "sample.c3d"
+    return send_file(sample_file, attachment_filename=sample_file, as_attachment=True)
+
 if __name__ == "__main__":
+    # 서버 실행
     app.run(debug=True)
     # host 등을 직접 지정하고 싶다면
     # app.run(host="127.0.0.1", port="5000", debug=True)
